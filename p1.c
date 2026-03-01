f86f311393af647fe102eae2173761e8ec7e1d9c
harsha2006-10 <hvardhann03@gmail.com>
2026-03-01 09:52:32 +0530

    squre for the user input

---
+++ p1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a , b, c;
+    printf("the a is : ");
+    scanf("%d",&a);
+    printf("the b is : ");
+    scanf("%d",&b);
+    c = a + b;  
+    printf("the addtion of the A + B is %d\n",c);
+    int d, square;
+    printf("value of d is :");
+    scanf("%d",&d);
+    square= d *d;
+    printf("the squre of the given number is %d\n",square);
+    return 0;
+}
